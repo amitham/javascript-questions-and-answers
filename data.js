f2eedb02b80fd21eddf5ac7674cc97a404b4f9df
amithap <amithap@embla.asia>
2022-08-13 23:11:35 +0530

    updated

--- data.js
+++ data.js
@@ -26,17 +26,6 @@ const allQuestionsAndAnswers = [
     {
         question: "What is JavaScript?",
         answer: "JavaScript is a scripting/programming langauge which we can use to build front-end and back-end web application"
-    },
-    {
-        question: "What is JavaScript?",
-        answer: "JavaScript is a scripting/programming langauge which we can use to build front-end and back-end web application"
-    },
-    {
-        question: "What is JavaScript?",
-        answer: "JavaScript is a scripting/programming langauge which we can use to build front-end and back-end web application"
-    },
-    {
-        question: "What is JavaScript?",
-        answer: "JavaScript is a scripting/programming langauge which we can use to build front-end and back-end web application"
     }
 ]
+  
