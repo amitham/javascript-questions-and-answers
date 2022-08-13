ab695c88434861cd20ae58c0c09bcc1ac740e670
Amitha <amitha4g@gmail.com>
2022-08-14 00:40:17 +0530

    Update data.js
    q3

--- data.js
+++ data.js
@@ -8,8 +8,8 @@ const allQuestionsAndAnswers = [
         answer: "isNan is a function which return true/false base on the argument that we passed to it. if the argument is not a number then it returns true otherwise return false"
     },
     {
-        question: "What is JavaScript?",
-        answer: "JavaScript is a scripting/programming langauge which we can use to build front-end and back-end web application"
+        question: "Explain imutability and mutability with details",
+        answer: "Imutable object is an object which we cannot modify the state of it after it is created mutable object is otherway around we can change the state evern after we create the object. In javascript mutability is bad, because it can leads to errors in code and give hrad time to track it down. So, in order to get this done we can use higher order functions like map,reduce,filter,etc to create new copy of those data/objects. But this can cost us time and space. But we can use persistent data structures for efficient immutability. (there are some libraries already implemented using persistent data structures. ex: immutable.js)
     },
     {
         question: "What is JavaScript?",
