099adb2ac0f7bff20e4a355527ebe94cc1d72804
Amitha <amitha4g@gmail.com>
2022-08-13 23:33:54 +0530

    Update data.js
    q2

--- data.js
+++ data.js
@@ -4,8 +4,8 @@ const allQuestionsAndAnswers = [
         answer: "JavaScript is a dynamic type programming langauge which we can use to build front-end and back-end web application"
     },
     {
-        question: "What is JavaScript?",
-        answer: "JavaScript is a scripting/programming langauge which we can use to build front-end and back-end web application"
+        question: "What is the use of isNaN function?",
+        answer: "isNan is a function which return true/false base on the argument that we passed to it. if the argument is not a number then it returns true otherwise return false"
     },
     {
         question: "What is JavaScript?",
